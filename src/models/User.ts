9becad3a943bc17fe4166363fbd33ed646c904a6
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-26 17:27:47 -0400

    created User.ts for user model with mongodb

---
+++ src/models/User.ts
@@ -0,0 +1,49 @@
+import { Schema, model, type Document } from 'mongoose';
+
+interface IUser extends Document {
+    username: string;
+    email: string;
+    thoughts: Schema.Types.ObjectId[];
+    friends: Schema.Types.ObjectId[];
+}
+
+const userSchema = new Schema<IUser>({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'thuoghts',
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        }
+    ]
+},
+{
+    toJSON: {
+        virtuals: true,
+    }
+}
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model<IUser>('User', userSchema);
+
+export default User;
