40a6c2d67ad2edfcaaeda450afb942996fdc04b8
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-27 18:54:41 -0400

    created routes for thoughts methods

---
+++ src/routes/api/thoughtRoutes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+import {
+    getThoughts, getSingleThought, createThought, updateThought, deleteThought, addReaction, removeReaction
+} from '../../controllers/thoughtController';
+
+const router = Router();
+
+// /api/thoughts
+router.route('/')
+    .get(getThoughts)
+    .post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought)
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thought:id/reactions')
+    .post(addReaction)
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId')
+    .delete(removeReaction)
