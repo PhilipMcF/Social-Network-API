73d6878bde3b438419833bd679859b792db415c9
Philip M DellLaptop2024 <philipsm1998@gmail.com>
2024-10-28 19:25:04 -0400

    updated addFriend & removeFriend ; will add each user to each others friend array and same for removing

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -76,17 +76,23 @@ export const deleteUser = async (req: Request, res: Response) => {
 
 export const addFriend = async (req: Request, res: Response) => {
     try {
-        const user = await User.findOneAndUpdate(
+        const user1 = await User.findOneAndUpdate(
             { _id: req.params.userId },
             { $addToSet: { friends: req.body.friendId } },  //may need to be changed
             { runValidators: true, new: true }
         );
 
-        if (!user){
-            res.status(404).json({ message: 'No user with this ID' });
+        const user2 = await User.findOneAndUpdate(
+            { _id: req.body.friendId },
+            { $addToSet: { friends: req.params.userId } },  //may need to be changed
+            { runValidators: true, new: true }
+        )
+
+        if (!user1 || !user2){
+            res.status(404).json({ message: 'Invalid ids received.' });
         }
         else {
-            res.status(200).json(user);
+            res.status(200).json({ message: 'Friend added' });
         }
     }
     catch (err) {
@@ -96,17 +102,23 @@ export const addFriend = async (req: Request, res: Response) => {
 
 export const removeFriend = async (req: Request, res: Response) => {
     try {
-        const user = await User.findOneAndUpdate(
+        const user1 = await User.findOneAndUpdate(
             { _id: req.params.userId },
             { $pull: { friends: req.params.friendId } },   //may need to be changed
             { runValidators: true, new: true }
         );
 
-        if (!user){
+        const user2 = await User.findOneAndUpdate(
+            { _id: req.params.friendId },
+            { $pull: { friends: req.params.userId } },   //may need to be changed
+            { runValidators: true, new: true }
+        )
+
+        if (!user1 || !user2){
             res.status(404).json({ message: 'No user with this ID' });
         }
         else {
-            res.status(200).json(user);
+            res.status(200).json({ message: 'Friend removed' });
         }
     }
     catch (err) {
